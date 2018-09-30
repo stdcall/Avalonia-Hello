cdf7b9fb644c63ce9c2f55d90761774ede70105a
Nikolay Khodyunya <nickolayho@gmail.com>
2018-09-30 22:03:46 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using MyApp.ViewModels;
+using MyApp.Views;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MainWindowViewModel();
+            BuildAvaloniaApp().Start<MainWindow>(() => context);
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+namespace MyApp.ViewModels
+{
+
+    
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+        public ReactiveCommand TestCommand { get; set;}
+
+        public MainWindowViewModel()
+        {
+            TestCommand = ReactiveCommand.Create(() => {});
+        }
+    }
+}
